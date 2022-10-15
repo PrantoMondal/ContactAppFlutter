4a77596a5643cf0b9f675bc65c62a1bb84da847b
PrantoMondal <prantomondal517@gmail.com>
2022-10-15 23:12:08 +0600

    progress bar added in details page

--- lib/pages/contact_details_page.dart
+++ lib/pages/contact_details_page.dart
@@ -1,4 +1,9 @@
+import 'package:contactapp/model/contact_model.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+
+import '../provider/contact_provider.dart';
+
 
 class ContactDetailsPage extends StatefulWidget {
   const ContactDetailsPage({Key? key}) : super(key: key);
@@ -20,6 +25,26 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Contact Details'),),
+      body: Center(
+        child: Consumer<ContactProvider>(
+          builder:(context, provider, _) => FutureBuilder<ContactModel>(
+            future: provider.getContactsById(id!),
+            builder: (context,snapshot){
+              if(snapshot.hasData){
+                return ListView(
+                  children: [
+
+                  ],
+                );
+              }
+              if(snapshot.hasError){
+                return const Text('Failed to fetch data');
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
